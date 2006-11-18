59e9cbd283c9eb562d3dead3d6e5de74c3426d65
Etienne Kneuss <colder@php.net>
2006-11-18 15:02:50 +0000

    -New predefined derivatives: acos, asin, atan -New package2.xml file
    git-svn-id: http://svn.php.net/repository/pear/packages/Math_Derivative/trunk@223551 c90b9560-bf6c-de11-be94-00142212c4b1

--- Derivative.php
+++ Derivative.php
@@ -7,9 +7,9 @@
  *
  * PHP version 4 and 5
  * 
- * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
  * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
  * the PHP License and are unable to obtain it through the web, please
  * send a note to license@php.net so we can mail you a copy immediately.
  *
@@ -17,7 +17,7 @@
  * @package    Math_Derivative
  * @author     Etienne Kneuss <colder@php.net>
  * @copyright  1997-2005 The PHP Group
- * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
  * @version    CVS: $Id$
  * @link       http://pear.php.net/package/Math_Derivative
  */
@@ -98,12 +98,16 @@ class Math_Derivative {
      * @var array
      * @access protected
      */
-    var $_registeredFunctions = array('sin' => 'cos(arg)*d(arg)',
-                                      'cos' => '-sin(arg)*d(arg)',
-                                      'tan' => '1/cos(arg)^2*d(arg)',
-                                      'ln'  => 'd(arg)/(arg)',
-                                      'log' => 'd(arg)/(arg)',
-                                      'e'   => 'd(arg)*e(arg)');
+    var $_registeredFunctions = array('sin'  => 'cos(arg)*d(arg)',
+                                      'cos'  => '-sin(arg)*d(arg)',
+                                      'tan'  => '1/cos(arg)^2*d(arg)',
+                                      'ln'   => 'd(arg)/(arg)',
+                                      'log'  => 'd(arg)/(arg)',
+                                      'e'    => 'd(arg)*e(arg)',
+                                      'sqrt' => 'd((arg)^(1/2))',
+                                      'acos' => '-1/((1-(arg)^2)^(1/2))',
+                                      'asin' => '1/((1-(arg)^2)^(1/2))',
+                                      'atan' => '1/(1+(arg)^2)');
     
     // }}}
     // {{{ setVariableName([(string) $value])
